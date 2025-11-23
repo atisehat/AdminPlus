1c550c0a6c77535f17480be7bb382e223aa67019
Your Name <your.email@example.com>
2025-11-23 18:03:23 -0500

    Fix Date Calculator - remove dropdown wrapper and bottom right section border

--- tools/dateCalculator.js
+++ tools/dateCalculator.js
@@ -126,10 +126,8 @@ function createModalContent() {
    
          <div class="securityPopup-row">
             <div class="section1-row1" id="section1">
-                <div class="headerWrapper">
-                    <h3 style="margin-bottom: 12px;">System Schedule(s):</h3>                    
-                    <select id="holidayScheduleDropdown"></select> <!-- Directly embedded dropdown -->
-                </div>
+                <h3 style="margin-bottom: 12px;">System Schedule(s):</h3>                    
+                <select id="holidayScheduleDropdown"></select>
                 <div class="holidaysList" id="holidaysList"></div>     			      
             </div>
             <div class="section1-row1" id="section2">
